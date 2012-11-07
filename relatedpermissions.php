a1d2ad9cd219ba91f7f387d9d7174f6c6cdaf4d2
eileen <eileen@mcnaughty.com>
2012-11-07 14:49:38 +1300

    loadable but functions not called

--- relatedpermissions.php
+++ relatedpermissions.php
@@ -6,6 +6,7 @@ require_once 'relatedpermissions.civix.php';
  * Implementation of hook_civicrm_config
  */
 function relatedpermissions_civicrm_config(&$config) {
+  dpm("config");
   _relatedpermissions_civix_civicrm_config($config);
 }
 
@@ -67,4 +68,63 @@ function relatedpermissions_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) 
  */
 function relatedpermissions_civicrm_managed(&$entities) {
   return _relatedpermissions_civix_civicrm_managed($entities);
+}
+/*
+ * Implement WHERE Clause - we find the contacts for whom this contact has permission and
+ * specifically give permission to them
+ */
+function relatedpermissions_civicrm_aclWhereClause($type, &$tables, &$whereTables, &$contactID, &$where) {
+dpm("here");
+  if (! $contactID) {
+    return;
+  }
+/*  $relationships = _relatedpermissions_get_permissionedrelatedcontacts( $contactID );
+  if(empty($relationships )){
+    return;
+  }
+  */
+  if(!empty($where)){
+    $clause[] = $where;
+  }
+  foreach ($relationships as $rel){
+  $tables ['civicrm_relationship_perm_a'] = $whereTables ['civicrm_relationship_perm_a'] =
+    "LEFT JOIN civicrm_relationship civicrm_relationship_perm_a}
+     ON (civicrm_relationship_perm_a.contact_id_a = {$contactID} AND abagroup{$rel['contact_id_b']}.contact_id_b = {$rel['contact_id_b']})
+";
+  $clause[] = "(abagroup{$rel['contact_id_b']}.start_date IS NULL OR abagroup{$rel['contact_id_b']}.start_date < NOW())
+    AND abagroup{$rel['contact_id_b']}.is_active = 1
+    AND (abagroup{$rel['contact_id_b']}.end_date IS NULL OR abagroup{$rel['contact_id_b']}.end_date > NOW())
+  ";
+  }
+  dpm($tables);
+  dpm($whereTables);
+  dpm($where);
+  $where = implode(' OR ', $clause);
+ // return TRUE;
+}
+
+/**
+ * Implementation of hook_civicrm_config
+ */
+function relatedpermissions_civicrm_buildForm($formName, &$form ) {
+  dpm("I ran");
+}
+
+
+function _relatedpermissions_get_permissionedrelatedcontacts($contact_id) {
+  $params = array(
+      'contact_id_a' => $contact_id,
+      'version' => 3,
+      'relationship_type_id' => array('IN' => array(22,35)),
+      'is_active' => 1,
+      'sequential' => 1,
+      'filters' => array(
+          'is_current' => 1,
+      )
+  );
+
+  $relationships = civicrm_api( 'relationship', 'get', $params );
+  if (empty( $relationships ['is_error'] ) && $relationships ['count']) {
+    return $relationships ['values'];
+  }
 }
